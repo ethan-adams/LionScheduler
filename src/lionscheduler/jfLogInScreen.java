12ddeda63b2eb356d9f4a19e5fc7c5b074edb186
ejb5674 <ejb5674@BKGAIGE205-11.win.psu.edu>
2019-03-21 12:11:09 -0400

    Added logo

--- src/lionscheduler/jfLogInScreen.java
+++ src/lionscheduler/jfLogInScreen.java
@@ -39,7 +39,7 @@ public class jfLogInScreen extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Penn State Berks - Lion Scheduler");
 
-        ImageIcon lionSchedulerLogo = new ImageIcon("images\\LionSchedulerLogo.png");
+        ImageIcon lionSchedulerLogo = new ImageIcon("src\\images\\LionSchedulerLogo.png");
         jlLogoLabel.setIcon(lionSchedulerLogo);
 
         jlUserName.setText("UserName:");
